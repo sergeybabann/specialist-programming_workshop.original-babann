c92e82e92616442b511666eaf589c803a5c1ad8e
sergeybabann <sergeybabann@yandex.ru>
2024-11-19 22:26:22 +0300

    1 day 1 video, time stop: 1.29.40

--- src/java11/day11.java
+++ src/java11/day11.java
@@ -1,7 +1,21 @@
 package java11;
 
-public class day11 {
-    public static void main (String[] args) {
+import java.util.Scanner; // library connection
 
+public class day11 {
+    public static void main (String[] args) { // entry point
+        Scanner scan = new Scanner(System.in); // Scanner - object type, scan - object name
+
+        int a,b,c; // a - variable, int - type
+
+        System.out.println("input 3 int:"); // print the text - input 3 int
+        a = scan.nextInt(); // a - variable, scan - object, nextInt() - method
+        b = scan.nextInt();
+        c = scan.nextInt();
+
+        int summa;
+        summa = a + b + c; // the result of adding these variables is added to the variable sum
+
+        System.out.println(summa); // print the result of addition
     }
 }
---
+++ src/java11/day11_2.java
@@ -0,0 +1,26 @@
+package java11;
+
+import java.util.Scanner; // library connection
+
+public class day11_2 {
+    public static void main (String[] args) { // entry point
+        Scanner scan = new Scanner(System.in); // Scanner - object type, scan - object name
+
+        double a,b,c; // a - variable, double - type
+
+        System.out.println("input 3 double:"); // print the text - input 3 double
+        a = scan.nextDouble(); // a - variable, scan - object, nextDouble() - method,
+        b = scan.nextDouble();
+        c = scan.nextDouble();
+
+        double summa;
+        summa = a + b + c; // the result of adding these variables is added to the variable summa
+
+        System.out.println(summa); // print the result of addition
+
+        String name;
+        System.out.println("input name:");
+        name = scan.next();
+        System.out.println(name);
+    }
+}
