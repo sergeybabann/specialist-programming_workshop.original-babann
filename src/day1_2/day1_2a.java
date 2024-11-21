3cb7dea2222936f993ddff84ed9a3b8a95fbc327
sergeybabann <sergeybabann@yandex.ru>
2024-11-21 18:31:09 +0300

    Lesson stop: 1 day | 1 video stop: 2.13.48

--- src/day1_2/day1_2a.java
+++ src/day1_2/day1_2a.java
@@ -1,5 +1,7 @@
 package day1_2;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class day1_2a {
@@ -21,5 +23,34 @@ public class day1_2a {
         int a3, b3 = 5, c3 = -2;
         a3 = +(c3 - b3);
         System.out.println(a3); // result -7
+
+        int a4, b4 = 5, c4 = -2;
+        a4 = -(c4 - b4);
+        System.out.println(a4); // result 7
+
+        int a5, b5 = 5, c5 = -2;
+        a5 = -c5;
+        a5++; // a5 = a5 + 1;
+        System.out.println(a5); // result 3
+
+        int a6, b6 = 5, c6 = -2;
+        a6 = -c6;
+        b6 = a6++; // b6 = a6 | a6 + 1 |
+        System.out.println(b6); // result 2
+
+        int a7, b7 = 5, c7 = -2;
+        a7 = -c7;
+        b7 = ++a7; // a7 = a7 + 1 | b7 = a7
+        System.out.println(b7); // result 3
+
+        int a8, b8 = 5, c8 = -2;
+        a8 = -c8;
+        b8 = a8--; // b8 = a8 | a8--
+        System.out.println(b8); // result 2
+
+        int a9, b9 = 5, c9 = -2;
+        a9 = -c9;
+        b9 = --a9; // --a9 | b9 = a9
+        System.out.println(b9); // result 1
     }
 }
