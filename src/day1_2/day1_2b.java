f897eebb356acbb18dde26c4eab9d33f99320715
sergeybabann <sergeybabann@yandex.ru>
2024-12-20 00:20:34 +0300

    adds

--- src/day1_2/day1_2b.java
+++ src/day1_2/day1_2b.java
@@ -14,9 +14,25 @@ public class day1_2b {
         System.out.println(a);
         System.out.println(b);
 
-        boolean f1 = true, f2 = false;
-        f1 = !f1 || f2;
-        System.out.println(f1);
+        boolean f1 = true, f2 = true;
+        f1 = !f2;
+        System.out.println(f1); // result false
+
+        boolean g1 = true, g2 = true;
+        g1 = g1 && g2;
+        System.out.println(g1); // result true
+
+        boolean h1 = true, h2 = false;
+        h1 = h1 && h2;
+        System.out.println(h1); // result false
+
+        boolean i1 = true, i2 = false;
+        i1 = i1 || i2;
+        System.out.println(i1); // result true
+
+        boolean j1 = true, j2 = false;
+        j1 = !j1 || j2;
+        System.out.println(j1); // result false
 
     }
 
